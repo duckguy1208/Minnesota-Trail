caea3d2fc6f886c9ec7d5a34c0b84fd34ede1bef
Tieler Kayfes <100615112+duckguy1208@users.noreply.github.com>
2026-03-04 17:34:45 +0000

    started work on game

--- Main.py
+++ Main.py
@@ -1,1 +1,21 @@
+import random
 
+overheating = 100
+hot = 80
+warm = 70
+fair = 60
+cold = 
+
+temp = warm
+
+while running == true:
+
+    def frostbite():
+        if temp == cold or freezing:
+            frost_chance = random.int(1, 10)
+            if frost_chance == 1:
+                print("you have frostbite")
+            else:
+                 print("You Are Cold")
+                
+                
