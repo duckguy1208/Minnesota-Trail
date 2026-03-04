a365b72d781efe555bd773a647867b6d6839eecd
Tieler Kayfes <100615112+duckguy1208@users.noreply.github.com>
2026-03-04 18:55:00 +0000

    set frame

--- Main.py
+++ Main.py
@@ -1,21 +1,60 @@
 import random
 
-overheating = 100
-hot = 80
-warm = 70
-fair = 60
-cold = 
 
-temp = warm
 
-while running == true:
+player = input("Enter Your Name: ")
+traveler1 = input("Enter The First Travelers Name: ")
+traveler2 = input("Enter The Second Travelers Name: ")
+traveler3 = input("Enter The Third Travelers Name: ")
+traveler4 = input("Enter The Forth Travelers Name: ")
+traveler5 = input("Enter The Fifth Travelers Name: ")
 
-    def frostbite():
-        if temp == cold or freezing:
-            frost_chance = random.int(1, 10)
-            if frost_chance == 1:
-                print("you have frostbite")
+location = "Albert Lea"
+player_heath = 100
+player_hunger = 0
+player_thirst = 0
+player_stamina = 100
+
+
+running = True
+
+while running == True:
+    def status():
+        print(location)
+        print("Health: " + str(player_heath))
+        print("Hunger: " + str(player_hunger))
+        print("Thirst: " + str(player_thirst))
+        print("Stamina: " + str(player_stamina))    
+
+    def player_turn():
+        action = input("What Do You Want To Do? ").lower().strip()
+        if action == "travel":
+            player_stamina =- 10
+            print("Travel")
+
+        elif action == "rest":
+            time = input("How Long Do You Want To Rest For? ")
+            player_stamina =+ time 
+            if player_stamina >= 85:
+                print("You Feel Well Rested")
+            elif player_stamina <= 85:
+                print("You Feel Rested")
+            elif player_stamina <= 50:
+                print("You Are Still Tired")
             else:
-                 print("You Are Cold")
-                
-                
+                print("You Are Super Tired")
+
+        elif action == "hunt":
+            print("hunt")
+
+        elif action == "status":
+            status()
+
+        elif action == "quit":
+            print("Thanks For Playing!")
+            running = False
+        else:
+            print("Invalid Try Again")
+
+    player_turn()
+    
