813245f49233f65f4700445f3fd57196830ad032
Tieler Kayfes <100615112+duckguy1208@users.noreply.github.com>
2026-03-22 22:11:22 -0500

    Create TODO.py

---
+++ TODO.py
@@ -0,0 +1,11 @@
+[x] make route
+[x] starting classes
+[x] attributes
+[x] player actions
+[x] shopping function
+[] landmarks
+[] random events (while traveling and hunting)
+[] default attributes to party members
+[] party members affected by environment
+[] curing ailments
+[] hunting mini game (maybe)
